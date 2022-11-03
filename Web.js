1a9fe2dcedebfaa5edb3ffaefba36dd4df750bf3
Lucky Chouhan <luckychouhan369@gmail.com>
2022-11-03 22:52:37 +0530

    Created Public folder to serve static files, connected to MongoDB using Mongoose & Making BlogSchema in BLogSchema.js for that I've made a separate folder named DataBase

--- Web.js
+++ Web.js
@@ -1,15 +1,40 @@
 const express = require('express');
-
+// Middleware
 const morgan = require('morgan')
+// mongoose
+const mongoose = required('mongoose')
+// Getting MongoURI ( I'm not giving my Key you can get yours from MongoDB Atlas )
+const { MongoDB_Connection } = require('./Keys/MongoURI')
 
 const app = express();
 
+// using PORT to serve our website otherwise 3000
+const PORT = process.env.PORT || 3000;
+
+// connecting to MongoDB Atlas with the help of Mongoose
+const URI = MongoDB_Connection;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
+        console.log(`Connected to MongoDB Atlas...`);
+        app.listen(PORT, () => {console.log(`server running on ${PORT}...`);})
+    })
+
+
+// Middlewares
 app.use(morgan('dev'))
+app.use(express.json())
+
+// Setting up view engine
 app.set('view engine', 'ejs')
+
+// serving static files like 'js, css that are required for our web pages'
+app.use(express.static('Public'))
 
 app.get('/', (req, res) => {
     // res.send('Hello world! This is my blog website.')
     res.render('Home')
 })
 
-app.listen(3000, () => {console.log(`server running...`);})
+app.get('/create', (req, res) => {
+    res.render('CreateBlog')
+})
