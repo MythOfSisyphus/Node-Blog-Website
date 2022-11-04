26b99d79de160c33eeca5f478dd5daf06196ce99
Lucky Chouhan <luckychouhan369@gmail.com>
2022-11-04 10:00:24 +0530

    deleted sensitive data folder, because it is not ignored by git although I added that folder's name in .gitignore

--- Keys/MongoURI.js
+++
@@ -1,3 +0,0 @@
-const MongoDB_Connection = 'mongodb+srv://luckychouhan92:luckyAtlas1234@learning.uksgmui.mongodb.net/Node_Blog_Web?retryWrites=true&w=majority';
-
-module.exports = { MongoDB_Connection }
